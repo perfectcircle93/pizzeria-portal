55ee85b3db4b27a89e6a56d32a6ed84cbb862a1c
Magdalena Świderska <m.swiderska@outlook.com>
2020-08-24 14:41:27 +0200

    Add routing to views pages

--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import styles from './Tables.module.scss';
+import {Link} from 'react-router-dom';
 
 const Tables = () => (
   <div className={styles.component}>
     <h2>Tables</h2>
+    <Link to={`${process.env.PUBLIC_URL}/tables/booking/new`}>New Booking</Link>
+    <Link to={`${process.env.PUBLIC_URL}/tables/booking/xyz098`}>Bookings</Link>
+    <Link to={`${process.env.PUBLIC_URL}/tables/events/new`}>New Event</Link>
+    <Link to={`${process.env.PUBLIC_URL}/tables/events/ghi123`}>Events</Link>
   </div>
 );
 
