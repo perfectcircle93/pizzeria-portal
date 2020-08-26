bb48b85e1cf58d16e593b878f5d8dc080f32b7e3
Magdalena Świderska <m.swiderska@outlook.com>
2020-08-26 08:51:35 +0200

    Add className to PageNav

--- src/components/layout/PageNav/PageNav.js
+++ src/components/layout/PageNav/PageNav.js
@@ -4,7 +4,7 @@ import Button from '@material-ui/core/Button';
 import styles from './PageNav.module.scss';
 
 const PageNav = () => (
-  <nav>
+  <nav className={styles.component}>
     <Button className={styles.link} component={NavLink} exact to={`${process.env.PUBLIC_URL}/`} activeClassName='active'>Home</Button>
     <Button className={styles.link} component={NavLink} to={`${process.env.PUBLIC_URL}/login`} activeClassName='active'>Login</Button>
     <Button className={styles.link} component={NavLink} to={`${process.env.PUBLIC_URL}/tables`} activeClassName='active'>Tables</Button>
