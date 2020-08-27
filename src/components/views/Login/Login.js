f0a264bbdc06d9607bcea5f73ac41bce781453c3
Magdalena Świderska <m.swiderska@outlook.com>
2020-08-27 11:39:14 +0200

    Add login view

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import styles from './Login.module.scss';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import {NavLink} from 'react-router-dom';
 
 const Login = () => (
-  <div className={styles.component}>
-    <h2>Login</h2>
-  </div>
+  <form className={styles.component}>
+    <TextField className={styles.input}
+      id="outlined-basic"
+      label="Login/e-mail"
+      variant="outlined" />
+
+    <TextField className={styles.input}
+      id="outlined-password-basic"
+      label="Password"
+      variant="outlined" />
+
+    <Button className={styles.button}
+      component={NavLink} to={`${process.env.PUBLIC_URL}/`}
+      variant="contained"
+      color="primary">
+      Login
+    </Button>
+
+
+  </form>
+
+
 );
 
 export default Login;
