0451cd609c7cf8ab3a4133882541707c68e2f787
Magdalena Świderska <m.swiderska@outlook.com>
2020-08-31 10:17:49 +0200

    Add changes

--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from './Tables.module.scss';
-//import {Link} from 'react-router-dom';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
